629bdfc3b389271dbda2bd9e283c69c799ea69e9
paulwoelfel <github@frig.at>
2013-06-19 16:51:35 +0200

    added command to fomrat text for Twiki

--- SublimeUtils.py
+++ SublimeUtils.py
@@ -1,5 +1,6 @@
 import sublime, sublime_plugin
 import sys, os
+import re
 
 if (os.path.join(os.path.dirname(__file__), "lib") in sys.path) == False :
 	sys.path.append(os.path.join(os.path.dirname(__file__), "lib"))
@@ -45,5 +46,22 @@ class CopyForRouterPasteCommand(sublime_plugin.TextCommand):
 		if filename.find(":/") == -1 : 
 			filename="flash://"+filename
 
-		sublime.set_clipboard("tclsh\nputs [ open \"%s\" w+ ] {%s}\ntclquit\n"%(filename,self.textToSend))
-		sublime.message_dialog("TCL commands to write to file %s have been copied to your clipboard.\nPaste to your telnet/SSH session to write the file."%(filename))
+		sublime.set_clipboard("tclsh\nputs [ open \"{0}\" w+ ] {{{1}}}\ntclquit\n".format(filename,self.textToSend))
+		sublime.message_dialog("TCL commands to write to file {0} have been copied to your clipboard.\nPaste to your telnet/SSH session to write the file.".format(filename))
+
+
+class FormatCsvForWikiCommand(sublime_plugin.TextCommand):
+	def run(self,edit):
+		text=''
+		if len(self.view.sel()) > 0:
+			for selection in self.view.sel():
+				self.view.replace(edit,selection,self.doReplace(self.view.substr(selection)))
+
+			
+		else:
+			self.view.replace(edit,sublime.Region(0,selv.view.size()),self.doReplace(self.view.substr(0)))
+
+
+	def doReplace(self,text):
+		return "|"+re.sub(r'(\n\r?)',r'|\1|',text.replace(';','|'))+"|"
+
